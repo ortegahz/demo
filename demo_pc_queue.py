678f7a670a963622dd7406772dbf68060c4a5882
manu <zxthz@126.com>
2021-11-09 17:56:56 +0800

    tiny

--- demo_pc_queue.py
+++ demo_pc_queue.py
@@ -1,7 +1,7 @@
 # author: zerg
 
 # libs
-from multiprocessing import Process, Queue, Value
+from multiprocessing import Process, Queue
 import cv2
 
 # params
@@ -10,7 +10,8 @@ name_window = 'frame'
 path_video = 'rtsp://192.168.3.34:554/live/ch4'
 
 
-def process_decoder(process_decoder_q, process_decoder_path_video, process_decoder_idx_frame, process_decoder_num_skip):
+def process_decoder(process_decoder_q, process_decoder_path_video, process_decoder_num_skip):
+    idx_frame = 0
     cap = cv2.VideoCapture(process_decoder_path_video)
     if cap.isOpened() is False:
         print("Error opening video steam")
@@ -18,9 +19,9 @@ def process_decoder(process_decoder_q, process_decoder_path_video, process_decod
     while cap.isOpened():
         ret, frame = cap.read()
         if ret is True:
-            process_decoder_idx_frame.value += 1
-            if process_decoder_idx_frame.value % process_decoder_num_skip == 0:
-                process_decoder_q.put([frame, process_decoder_idx_frame.value])
+            idx_frame += 1
+            if idx_frame % process_decoder_num_skip == 0:
+                process_decoder_q.put([frame, idx_frame])
             print('[process_decoder] number of frames in the queue: %d' % process_decoder_q.qsize())
         else:
             break
@@ -29,15 +30,14 @@ def process_decoder(process_decoder_q, process_decoder_path_video, process_decod
 
 
 if __name__ == '__main__':
-    q_frame = Queue()
-    idx_frame = Value('i', 0)
+    q_decoder = Queue()
 
-    p_decoder = Process(target=process_decoder, args=(q_frame, path_video, idx_frame, num_skip), daemon=True)
+    p_decoder = Process(target=process_decoder, args=(q_decoder, path_video, num_skip), daemon=True)
     p_decoder.start()
 
     cv2.namedWindow(name_window, cv2.WINDOW_NORMAL)
     while True:
-        item_frame = q_frame.get()
+        item_frame = q_decoder.get()
         frame = item_frame[0]
         cv2.imshow(name_window, frame)
         if cv2.waitKey(1) & 0xFF == ord('q'):

fbbd8c9b544370817b7d30e75dc9227da59337f9
manu <zxthz@126.com>
2021-11-11 10:15:16 +0800

    real time face recognition

--- demo_pc_queue.py
+++ demo_pc_queue.py
@@ -2,31 +2,80 @@
 
 # libs
 from multiprocessing import Process, Queue
+import numpy as np
 import cv2
 import process
-import numpy as np
+import glob
 import os
+import sys
 
 from retinaface import RetinaFace
+import face_model
+
+sys.path.append(os.path.join(os.path.dirname(__file__), 'common'))
+import face_preprocess
 
 # params
-num_skip = 5  # for speed reason
+num_skip = 6  # for speed reason
 name_window = 'frame'
-path_video = 'rtsp://192.168.3.34:554/live/ch4'
+# path_video = 'rtsp://192.168.3.34:554/live/ch4'
+path_video = 'rtsp://192.168.3.233:554/live/ch4'
 
 model_face_detect_path = './models/mobilenet_v1_0_25/retina'
 warmup_img_path = '/media/manu/samsung/pics/material3000_1920x1080.jpg'  # image size should be same as actual input
 gpuid = 0
-thresh = 0.8
+thresh = 0.5
 scales = [1.0]
 flip = False
 
+face_dataset_dir = '/media/manu/samsung/pics/人脸底图'
+model_face_recog_path = '/media/manu/intel/workspace/insightface_manu_subcenter/models/model,0'
+face_recog_sim_th = 0.5
+face_recog_dist_th = 2.0
+
 if __name__ == '__main__':
+    print('face detect init start ...')
     detector = RetinaFace(model_face_detect_path, 0, gpuid, 'net3')
     img = cv2.imread(warmup_img_path)
+    print('face detect init done')
+
+    print('face recog init start ...')
+    face_recog_dataset = []
+    model = face_model.FaceModel(gpuid, model_face_recog_path)
+    for path_img in glob.glob(os.path.join(face_dataset_dir, '*.jpg')):
+        _, img_name = os.path.split(path_img)
+        img_name = img_name.replace('.jpg', '')
+        stu_id, stu_name = img_name.split('_')
+        img = cv2.imread(path_img)
+        h, w, c = img.shape
+        max_l = max(h, w)
+        scales_reg = [1.0]
+        if max_l > 1920:  # can not detect faces on some large input images
+            scales_reg = [0.5]
+        faces, landmarks = detector.detect(img, thresh, scales=scales_reg, do_flip=flip)
+        assert len(faces) == 1  # TODO
+        # face align and feature extract
+        bbox = faces
+        points = np.squeeze(landmarks).transpose().reshape(1, 10)
+        bbox = bbox[0, 0:4]
+        points = points[0, :].reshape((2, 5)).T
+        img_aligned = face_preprocess.preprocess(img, bbox, points, image_size='112,112')
+        out_dir = '/home/manu/tmp'
+        out_path = os.path.join(out_dir, stu_name + '.jpg')
+        cv2.imwrite(out_path, img_aligned)
+        img_aligned = cv2.cvtColor(img_aligned, cv2.COLOR_BGR2RGB)
+        img_aligned = np.transpose(img_aligned, (2, 0, 1))
+        feat = model.get_feature(img_aligned)
+        item = (stu_id, stu_name, feat)
+        face_recog_dataset.append(item)
+        print('record student %s with id %s' % (stu_name, stu_id))
+    print('face recog init done')
+
+    print('warm up start ...')
     # warm up
     for _ in range(10):
         faces, landmarks = detector.detect(img, thresh, scales=scales, do_flip=flip)
+    print('warm up done')
 
     q_decoder = Queue()
     p_decoder = Process(target=process.process_decoder, args=(q_decoder, path_video, num_skip), daemon=True)
@@ -34,28 +83,59 @@ if __name__ == '__main__':
 
     cv2.namedWindow(name_window, cv2.WINDOW_NORMAL)
     cv2.resizeWindow(name_window, 960, 540)
+    face_recog_aligned_save_idx = 0
     while True:
         item_frame = q_decoder.get()
-        frame_4k = item_frame[0]
-        frame_2k = cv2.resize(frame_4k, (0, 0), fx=0.5, fy=0.5, interpolation=cv2.INTER_NEAREST)
-        faces, landmarks = detector.detect(frame_2k, thresh, scales=scales, do_flip=flip)
+        frame = item_frame[0]
+        h, w, c = frame.shape
+        if w > 1920:
+            scales = [0.5]
+        faces, landmarks = detector.detect(frame, thresh, scales=scales, do_flip=flip)
         if faces is not None:
             print('find', faces.shape[0], 'faces')
+
+            # recognition
+            for i in range(faces.shape[0]):
+                box = faces[i].astype(int)
+                bbox = faces[i]
+                landmarks_recog = landmarks[i]
+                points = landmarks_recog.transpose().reshape(1, 10)
+                bbox = bbox[0:4]
+                points = points[0, :].reshape((2, 5)).T
+                img_aligned = face_preprocess.preprocess(frame, bbox, points, image_size='112,112')
+                img_aligned_write = img_aligned
+                img_aligned = cv2.cvtColor(img_aligned, cv2.COLOR_BGR2RGB)
+                img_aligned = np.transpose(img_aligned, (2, 0, 1))
+                feat = model.get_feature(img_aligned)
+                for stu_id, stu_name, feat_ref in face_recog_dataset:
+                    # sim = np.dot(feat_ref, feat.T)  # sim is wired
+                    dist = np.sum(np.square(feat_ref - feat))
+                    if dist < face_recog_dist_th:
+                        info = stu_name + ' with dist ' + '%f' % dist
+                        img = cv2.putText(frame, info, (box[0], box[1]), cv2.FONT_HERSHEY_SIMPLEX, 1.2, (0, 255, 0), 2)
+                        out_dir = './'
+                        out_path = os.path.join(out_dir,
+                                                '%s_%d_%f' % (stu_name, face_recog_aligned_save_idx, dist) + '.jpg')
+                        cv2.imwrite(out_path, img_aligned_write)
+                        face_recog_aligned_save_idx += 1
+
+            # plot
             for i in range(faces.shape[0]):
                 # print('score', faces[i][4])
-                box = faces[i].astype(np.int)
+                box = faces[i].astype(int)
                 # color = (255,0,0)
                 color = (0, 0, 255)
-                cv2.rectangle(frame_2k, (box[0], box[1]), (box[2], box[3]), color, 2)
+                cv2.rectangle(frame, (box[0], box[1]), (box[2], box[3]), color, 2)
                 if landmarks is not None:
-                    landmark5 = landmarks[i].astype(np.int)
+                    landmark5 = landmarks[i].astype(int)
                     # print(landmark.shape)
                     for l in range(landmark5.shape[0]):
                         color = (0, 0, 255)
                         if l == 0 or l == 3:
                             color = (0, 255, 0)
-                        cv2.circle(frame_2k, (landmark5[l][0], landmark5[l][1]), 1, color, 2)
-        cv2.imshow(name_window, frame_2k)
+                        cv2.circle(frame, (landmark5[l][0], landmark5[l][1]), 1, color, 2)
+
+        cv2.imshow(name_window, frame)
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
 

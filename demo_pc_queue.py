559a5ca818bc08b9ab4c8ef04b7f26f88a0077db
manu <zxthz@126.com>
2021-11-15 16:38:31 +0800

    tiny

--- demo_pc_queue.py
+++ demo_pc_queue.py
@@ -138,8 +138,7 @@ if __name__ == '__main__':
                 # print('score', faces[i][4])
                 box = faces[i].astype(int)
                 # color = (255,0,0)
-                color = (0, 0, 255)
-                cv2.rectangle(frame, (box[0], box[1]), (box[2], box[3]), color, 2)
+                cv2.rectangle(frame, (box[0], box[1]), (box[2], box[3]), (0, 255, 0), 2)
                 if landmarks is not None:
                     landmark5 = landmarks[i].astype(int)
                     # print(landmark.shape)

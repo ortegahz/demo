dd6cf87adad608d34e3ac500e60de163c43ce6d2
manu <zxthz@126.com>
2022-07-12 10:43:14 +0800

    demo_bgrm.py

---
+++ classes.py
@@ -0,0 +1,69 @@
+from threading import Thread, Lock
+import time
+import cv2
+
+
+class Decoder:
+    def __init__(self, path):
+        self.capture = cv2.VideoCapture(path)
+        self.width = int(self.capture.get(cv2.CAP_PROP_FRAME_WIDTH))
+        self.height = int(self.capture.get(cv2.CAP_PROP_FRAME_HEIGHT))
+        self.success_reading, self.frame = self.capture.read()
+        self.read_lock = Lock()
+        self.process = Thread(target=self.__update, args=())
+        self.process.daemon = True
+        self.process.start()
+
+    def __update(self):
+        while self.success_reading:
+            grabbed, frame = self.capture.read()
+            with self.read_lock:
+                self.success_reading = grabbed
+                self.frame = frame
+
+    def read(self):
+        with self.read_lock:
+            frame = self.frame.copy()
+        return frame
+
+    def __exit__(self):
+        self.capture.release()
+
+
+class FPSTracker:
+    def __init__(self, ratio=0.5):
+        self._last_tick = None
+        self._avg_fps = None
+        self.ratio = ratio
+
+    def tick(self):
+        if self._last_tick is None:
+            self._last_tick = time.time()
+            return None
+        t_new = time.time()
+        fps_sample = 1.0 / (t_new - self._last_tick)
+        self._avg_fps = self.ratio * fps_sample + (
+                1 - self.ratio) * self._avg_fps if self._avg_fps is not None else fps_sample
+        self._last_tick = t_new
+        return self.get()
+
+    def get(self):
+        return self._avg_fps
+
+
+class Displayer:
+    def __init__(self, title, width=None, height=None, show_info=True):
+        self.title, self.width, self.height = title, width, height
+        self.show_info = show_info
+        self.fps_tracker = FPSTracker()
+        cv2.namedWindow(self.title, cv2.WINDOW_NORMAL)
+        if width is not None and height is not None:
+            cv2.resizeWindow(self.title, width, height)
+
+    def step(self, image):
+        fps_estimate = self.fps_tracker.tick()
+        if self.show_info and fps_estimate is not None:
+            message = f"{int(fps_estimate)} fps | {self.width}x{self.height}"
+            cv2.putText(image, message, (10, 40), cv2.FONT_HERSHEY_SIMPLEX, 1.0, (0, 0, 255))
+        cv2.imshow(self.title, image)
+        return cv2.waitKey(1) & 0xFF

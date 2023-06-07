943ce66f6154b47defb533b9db377c70fb6f7318
manu <zxthz@126.com>
2023-06-07 14:17:25 +0800

    demo for phone detection

--- utils/decoder.py
+++ utils/decoder.py
@@ -1,25 +1,30 @@
 import cv2
 import time
+import logging
 
 
-def process_decoder(path_video, buff_len, arr_frames):
+def process_decoder(path_video, queue, buff_len=5):
     idx_frame = 0
     cap = cv2.VideoCapture(path_video)
-    if cap.isOpened() is False:
-        print("Error opening video steam")
+    fps = cap.get(cv2.CAP_PROP_FPS)
+    fc = cap.get(cv2.CAP_PROP_FRAME_COUNT)
+    if not cap.isOpened():
+        logging.error("Error opening video steam")
 
+    t_last = time.time()
     while cap.isOpened():
         ret, frame = cap.read()
-        if ret is True:
-            idx_frame += 1
-            if len(arr_frames) == buff_len * 2:
-                arr_frames.pop(0)
-                print('[process_decoder] dropping frame !!!')
-            arr_frames.append([idx_frame, frame])
-            print('[process_decoder] number of frames in the arr_frames: %d' % len(arr_frames))
-        else:
+        if not ret:
             break
 
-        time.sleep(0.020)
+        idx_frame += 1
+        if queue.qsize() > buff_len:
+            queue.get()
+            logging.info('dropping frame !!!')
+        queue.put([idx_frame, frame, fc])
+
+        while time.time() - t_last < 1. / fps:
+            time.sleep(0.001)
+        t_last = time.time()
 
     cap.release()

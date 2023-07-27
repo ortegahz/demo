35fefac75b9673931466e50524247ecd9e0ad2fe
manu <zxthz@126.com>
2023-07-27 16:09:52 +0800

    tiny

--- matlab/hisi_tool_yuv_420spnv12.m
+++ matlab/hisi_tool_yuv_420spnv12.m
@@ -3,7 +3,6 @@ clear; close all;
 
 %%
 fp = '/home/manu/nfs/mpp/sample/svp/nnie/data/nnie_image/yuv/out360.nv12';
-wc = 640; hc = 640;
 wi = 640; hi = 360;
 
 fid = fopen(fp);
@@ -20,24 +19,7 @@ R = Y + 1.4075 * (VR - 128);
 G = Y - 0.3455 * (UR - 128) - 0.7169 * (VR - 128);
 B = Y + 1.779 * (UR - 128);
 RGB = cat(3, R, G, B);
-figure; imshow(uint8(RGB));
-
-% TODO: wc must equal wi
-assert(wc == wi);
-CVS = zeros(1, wc*hc*3/2);
-CVS(1:wi*hi) = A(1:wi*hi);
-CVS(wc*hc+1:wc*hc+wi*hi/2) = A(wi*hi+1:wi*hi*3/2);
-Y = reshape(CVS(1:wc*hc), wc, hc)';
-UV = CVS(wc*hc+1:wc*hc*3/2);
-U = UV(1:2:end); V = UV(2:2:end);
-U = reshape(U, wc/2, hc/2)';
-V = reshape(V, wc/2, hc/2)';
-UR = imresize(U, 2); VR = imresize(V, 2);
-R = Y + 1.4075 * (VR - 128);
-G = Y - 0.3455 * (UR - 128) - 0.7169 * (VR - 128);
-B = Y + 1.779 * (UR - 128);
-RGB = cat(3, R, G, B);
-figure; imshow(uint8(RGB));
+imshow(uint8(RGB));
 
 
 

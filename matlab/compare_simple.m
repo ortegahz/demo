75fc5969efec51c9e155dea185fe15a61a63281b
manu <zxthz@126.com>
2023-12-11 16:35:59 +0800

    add cv_video_snapshot.py

--- matlab/compare_simple.m
+++ matlab/compare_simple.m
@@ -1,34 +1,35 @@
 clear; close all;
+h = 18; w = 34;
 
-% out_ref = load('/home/manu/tmp/pytorch_results.txt');
-out_ref = load('/home/manu/tmp/pytorch_outputs_rsn.txt');
-% out_ref = load('/home/manu/tmp/onnx_output_img_wa.txt');
-% out_ref = load('/home/manu/tmp/pytorch_output_dr_0.txt');
-% out_ref = load('/home/manu/tmp/rknn_output_dr_0.txt');
-% out_ref = load('/home/manu/tmp/pytorch_output_img_wa.txt');
-% out_ref = load('/home/manu/tmp/pytorch_outputs_reg_output_3.txt');
-% out_ref = load('/home/manu/tmp/pytorch_outputs_0.txt');
-% out_ref = load('/media/manu/kingstop/workspace/ONNXToCaffe/output/dump/layers/257_onnx.txt');
+% out_ref = load('/home/manu/nfs/data/python_img_.txt');
+% out_ref = load('/home/manu/nfs/data/python_img_gray.txt');
+% out_ref = load('/home/manu/nfs/data/python_img_now.txt');
+% out_ref = load('/home/manu/nfs/data/python_labels.txt');
+% out_ref = load('/home/manu/nfs/data/python_stats.txt');
+% out_ref = load('/home/manu/nfs/data/python_centroids.txt');
+% out_ref = load('/home/manu/nfs/data/python_mat200.txt');
+% out_ref = load('/home/manu/nfs/data/python_gaussian_one.txt');
+% out_ref = load('/home/manu/nfs/data/python_img_crop_fire.txt');
+% out_ref = load('/home/manu/nfs/data/python_x.txt');
+out_ref = load('/home/manu/nfs/data/python_y.txt');
 
-% out_q = load('/home/manu/tmp/results_rknn.txt');
-% out_q = load('/home/manu/tmp/onnx_outputs_rsn_outputs.txt');
-% out_q = load('/media/manu/kingstop/workspace/rknn-toolkit/examples/onnx/rsn/snapshot/fp32/images_2_out0_nhwc_1_256_192_3.tensor');
-% out_q = load('/home/manu/tmp/rknn_output_rsn_0_nq.txt');
-% out_q = load('/home/manu/tmp/rknn_output_rsn_0.txt');
-% out_q = load('/home/manu/tmp/rknn_output_dr_0.txt');
-% out_q = load('/home/manu/tmp/rknn_output_img_wa.txt');
-% out_q = load('/home/manu/tmp/rknn_output_0_nq.txt');
-% out_q = load('/home/manu/nfs/rv1126/install/rknn_yolov5_demo/rknn_output_real_kps_nq_0.txt');
-out_q = load('/home/manu/nfs/rv1126/install/rknn_yolov5_demo/rknn_output_real_kps_0.txt');
-% out_q = load('/home/manu/nfs/rv1126/install/rknn_yolov5_demo/rknn_output_featureMap_0.txt');
-% out_q = load('/home/manu/nfs/rv1126/install/rknn_yolov5_demo/rknn_output_input_data.txt');
-% out_q = load('/home/manu/tmp/rknn_output_7_nq.txt');
-% out_q = load('/home/manu/nfs/rv1126/install/rknn_yolov5_demo/rknn_output_real_nq_7.txt');
-% out_q = load('/home/manu/nfs/rv1126/install/rknn_yolov5_demo/rknn_output_real_7.txt');
-% out_q = load('/home/manu/tmp/onnx_outputs_2.txt');
-% out_q = load('/home/manu/nfs/mpp/sample/svp/nnie/seg0_layer127_output0_inst.linear.float');
-% out_q = load('/home/manu/nfs/mpp/sample/svp/nnie/seg0_layer124_output0_inst.linear.float');
-% out_q = load('/home/manu/nfs/mpp/sample/svp/nnie/seg0_layer72_output0_inst.linear.float');
+% figure;
+% imshow(reshape(out_ref, w, h)');
+
+% out_q = load('/home/manu/nfs/data/cpp_srcNormDebug.txt');
+% out_q = load('/home/manu/nfs/data/cpp_srcGray.txt');
+% out_q = load('/home/manu/nfs/data/cpp_bin.txt');
+% out_q = load('/home/manu/nfs/data/cpp_labels.txt');
+% out_q = load('/home/manu/nfs/data/cpp_stats.txt');
+% out_q = load('/home/manu/nfs/data/cpp_centroids.txt');
+% out_q = load('/home/manu/nfs/data/cpp_binSecond.txt');
+% out_q = load('/home/manu/nfs/data/cpp_gaussianOne.txt');
+% out_q = load('/home/manu/nfs/data/cpp_srcGray(expandRectOne).txt');
+% out_q = load('/home/manu/nfs/data/cpp_SrcSobelRoi.txt');
+out_q = load('/home/manu/nfs/data/cpp_SrcSobelRoiMixed.txt');
+
+% figure;
+% imshow(reshape(out_q, w, h)');
 
 % error = sum((out_ref - out_q) .^ 2) / length(out_ref);
 sim_cos = dot(out_ref, out_q) / (norm(out_ref) * norm(out_q));  
--- tools/cv_video_snapshot.py
+++ tools/cv_video_snapshot.py
@@ -1,6 +1,7 @@
-import cv2
-import logging
 import argparse
+import logging
+
+import cv2
 
 
 def set_logging():
@@ -10,7 +11,8 @@ def set_logging():
 
 def parse_args():
     parser = argparse.ArgumentParser('video player')
-    parser.add_argument('--path_in_video', default='/home/manu/视频/vlc-record-2023-11-29-15h24m48s-rtsp___172.20.20.181_ir_stream-.mp4')
+    parser.add_argument('--path_in_video',
+                        default='/home/manu/视频/vlc-record-2023-11-29-15h24m48s-rtsp___172.20.20.181_ir_stream-.mp4')
     return parser.parse_args()
 
 
@@ -25,8 +27,21 @@ def main():
              int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT)))
     n_frames = cap.get(cv2.CAP_PROP_FRAME_COUNT)
     logging.info(f'fps: {fps} size: {sizes} n_frames: {n_frames}')
-    if not cap.isOpened():
-        logging.error("error opening video steam")
+
+    cnt, idx = 0, 0
+    while cap.isOpened():
+        ret, frame = cap.read()
+        cnt += 1
+
+        if not cnt % 25 == 0:
+            continue
+
+        if frame.shape[0] != 1080 and frame.shape[1] != 1440:
+            frame = cv2.resize(frame, (1440, 1080), interpolation=cv2.INTER_CUBIC)
+
+        cv2.imwrite(f"/home/manu/tmp/snapshot/{idx}.bmp", frame)
+        idx += 1
+
     cap.release()
 
 
